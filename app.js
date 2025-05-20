801f99cbcb3dcb7e2048ad1fae18f391493b4055
Paulo Gabriel <gableit@outlook.com>
2025-05-20 11:37:59 -0300

    primeira commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let dia = prompt("Qual é o dia da semana?");
+if (dia == "Sábado") {
+    alert("Bom final de Semana!");
+} else if (dia == "domingo") {
+    alert("Bom final de semana!");
+} else {
+    alert("Boa Semana!");
+}
